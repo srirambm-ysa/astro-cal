d7732c33fd9ed1c1c6bdbaf23e18c4235584b292
Balaraman Sriram <srirambm@gmail.com>
2026-08-22 17:53:20 +0530

    chore: add favicon to help pages, fix worker handler, rename header

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    return new Response("Not Found", { status: 404 });
+    return env.ASSETS.fetch(request);
   },
 };
